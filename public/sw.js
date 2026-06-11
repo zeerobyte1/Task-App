6d612ba620d9a4fd5b12a67cd7cb324c2534cd5f
israfil Hussain <zerobyte1@gmail.com>
2026-06-11 14:48:51 +0530

    download problem

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "my-cache-v1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "/",
+        "/css/style.css",
+        "/js/script.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
